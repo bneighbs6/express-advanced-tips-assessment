9fb5fd59c10c4c1aaece1f94ae2aef0def83889a
Brett <brett_neighbors@yahoo.com>
2024-02-28 07:30:42 -0700

    Created list() fx and used it in ratings router

--- src/ratings/ratings.controller.js
+++ src/ratings/ratings.controller.js
@@ -1,2 +1,6 @@
 // Import ratings data as ratings
 const ratings = require("../data/ratings-data");
+
+function list(req, res) {
+    res.json({ data: ratings });
+}
--- src/ratings/ratings.router.js
+++ src/ratings/ratings.router.js
@@ -1,4 +1,8 @@
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
 const router = require("express").Router();
 
 // need to import controller when it has functioning code
+const controller = require("./ratings.controller").all(methodNotAllowed)
 
+router.route("/").get(controller.list);
