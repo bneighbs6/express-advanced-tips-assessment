36b98325bc56230b543cbf57cb554aa06a0e3387
Brett <brett_neighbors@yahoo.com>
2024-02-28 08:57:55 -0700

    Created ratingExists() midware fx. 3/15 tests pass

--- src/ratings/ratings.controller.js
+++ src/ratings/ratings.controller.js
@@ -5,6 +5,20 @@ function list(req, res) {
     res.json({ data: ratings });
 }
 
+// Middleware vailidation fx to verify rating exists
+function ratingExists(req, res, next) {
+    const { ratingId } = req.params;
+    const foundRating = ratings.find((rating) => rating.id === Number(ratingId));
+    if (foundRating) {
+        res.locals.rating = foundRating;
+        return next();
+    }
+    next({ 
+        status: 400,
+        message: `Rating id not found ${ratingId}`,
+    })
+}
+
 module.exports = {
     list,
 }
