ee8dde130956c532d9e2c415e64272cc988bd9d1
Brett <brett_neighbors@yahoo.com>
2024-02-28 07:04:52 -0700

    Created and exported methodNotAllowed file and fx

--- src/errors/methodNotAllowed.js
+++ src/errors/methodNotAllowed.js
@@ -1,9 +1,8 @@
-/* 
 function methodNotAllowed(req, res) {
     next({
         status: 405,
         message: `${req.method} not allowed for ${req.originalUrl}`,
     })
 }
+
 module.exports = methodNotAllowed
-*/
