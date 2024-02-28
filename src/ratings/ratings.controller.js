d85c69e0c3710bdc6581b0f3101f00b7475ce013
Brett <brett_neighbors@yahoo.com>
2024-02-28 07:34:48 -0700

    3. /ratings returns all ratings complete. 3/15 passing

--- src/ratings/ratings.controller.js
+++ src/ratings/ratings.controller.js
@@ -4,3 +4,7 @@ const ratings = require("../data/ratings-data");
 function list(req, res) {
     res.json({ data: ratings });
 }
+
+module.exports = {
+    list,
+}
--- src/ratings/ratings.router.js
+++ src/ratings/ratings.router.js
@@ -3,6 +3,8 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 const router = require("express").Router();
 
 // need to import controller when it has functioning code
-const controller = require("./ratings.controller").all(methodNotAllowed)
+const controller = require("./ratings.controller")
 
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).all(methodNotAllowed);
+
+module.exports = router; 
